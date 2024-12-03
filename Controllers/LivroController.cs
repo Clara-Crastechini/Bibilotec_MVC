e43a2e5262512f7e290b61b526774c7baaf747cd
Clara Crastechini <ccrastechini@gmail.com>
2024-12-03 11:51:43 -0300

    Comecando o método cadastrar

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -5,6 +5,7 @@ using Bibliotec.Contexts;
 
 namespace Bibliotec.Controllers;
 
+[Route("[controller]")]
 public class LivroController : Controller
 {
     private readonly ILogger<LivroController> _logger;
@@ -25,6 +26,7 @@ public class LivroController : Controller
         List<Livro> listalivros = context.Livro.ToList();
 
         // Verificar se o livro tem reserva ou nao
+        // ToDictionary (chave, valor)
         var livrosReservados = context.LivroReserva.ToDictionary(livro => livro.LivroID, livror => livror.DtReserva);
 
         ViewBag.Livros = listalivros;
@@ -33,10 +35,42 @@ public class LivroController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
+    [Route("Cadastro")]
+    // Metodo que retorna a tela de cadastro
+
+    public IActionResult Cadastro(){
+        ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
+
+        ViewBag.Categorias = context.Categoria.ToList(); 
+
         return View();
     }
+
+
+    // Metodo para cadastrar um livro
+    [Route("Cadastrar")]
+
+    public IActionResult Cadastrar(IFormCollection form){
+        Livro novoLivro = new Livro();
+        // O que meu usuario escrever no formulario sera atribuido ao novoLivro
+
+        novoLivro.Nome = form["Nome"].ToString();
+        novoLivro.Descricao = form["Descricao"].ToString();
+        novoLivro.Editora = form["Editora"].ToString();
+        novoLivro.Escritor = form["Escritor"].ToString();
+        novoLivro.Idioma = form["Idioma"].ToString();
+
+        // imagem 
+        context.Livro.Add(novoLivro);
+        context.SaveChanges();
+        return RedirectToAction("Cadastro");
+    }
+
+
+    // public IActionResult Privacy()
+    // {
+    //     return View();
+    // }
 
     // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     // public IActionResult Error()
