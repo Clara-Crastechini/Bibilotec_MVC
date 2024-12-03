0f6be89ad45c08d6a8e3a948cf10afccf1965cb0
Clara Crastechini <ccrastechini@gmail.com>
2024-12-03 16:46:34 -0300

    Terminando o cadastro do livro

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -59,12 +59,63 @@ public class LivroController : Controller
         novoLivro.Editora = form["Editora"].ToString();
         novoLivro.Escritor = form["Escritor"].ToString();
         novoLivro.Idioma = form["Idioma"].ToString();
+        // Trabalhar com imagens:
+        if(form.Files.Count > 0){
+            // Primeiro passo:
+                // Armazenaremos o arquivo enviado pelo usuario
+                var arquivo = form.Files[0];
+                
+            // Segundo passo: 
+                // Criar variave do caminho da minha pasta para colocar as fotos dos livros
+                var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Livros");
+
+                // Validaremos se a pasta que será armazenada as imagens, existe. Caso nao exista, criaremos uma nova pasta
+                if(!Directory.Exists(pasta)){
+                    // Criar pasta
+                    Directory.CreateDirectory(pasta);
+                }
+            // Terceiro passo: 
+                var caminho = Path.Combine(pasta, arquivo.FileName);
+                using (var stream = new FileStream(caminho, FileMode.Create)){
+                    //Copiou o arquivo para o meu diretorio
+                    arquivo.CopyTo(stream);
+                }
+
+                novoLivro.Imagem = arquivo.FileName;
+        } else{
+            novoLivro.Imagem= "padrão.png";
+        }
+
+
 
         // imagem 
         context.Livro.Add(novoLivro);
         context.SaveChanges();
-        return RedirectToAction("Cadastro");
+
+        // Segunda parte é adicionar dentro de LivroCategorias a categoria que pertence ao novoLivro
+
+        List<LivroCategoria> listaLivroCategorias = new List<LivroCategoria>();
+        // Lista as categorias
+
+        // Array que possui as categorias selecionadas pelo usuario 
+        string[] categoriasSelecionadas = form["Categoria"].ToString().Split(',');
+
+        foreach(string categoria in categoriasSelecionadas){
+            // categoria possui a informacao do id da categoria ATUAL selecionada
+            LivroCategoria livroCategoria = new LivroCategoria();
+            livroCategoria.CategoriaID = int.Parse(categoria);
+            livroCategoria.LivroID = novoLivro.LivroID;
+            listaLivroCategorias.Add(livroCategoria);
+
+        }
+
+        // peguei a colecao da listaLivroCategorias e coloquei na tabela LivroCategoria
+        context.LivroCategoria.AddRange(listaLivroCategorias);
+        context.SaveChanges();
+
+        return LocalRedirect("/Livro/Cadastro");
     }
+}
 
 
     // public IActionResult Privacy()
@@ -77,4 +128,4 @@ public class LivroController : Controller
     // {
     //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     // }
-}
+
